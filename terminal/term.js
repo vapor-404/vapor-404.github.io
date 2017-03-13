e95d76054d1c222fe4bc59790b700bd7831f44b6
xprmt <adrianhgarz@gmail.com>
2017-03-13 19:05:22 -0400

    Update term.js

--- terminal/term.js
+++ terminal/term.js
@@ -223,7 +223,6 @@ function history(input) {
 
 function help(input) {
     //add some kind of help for various functions (like rendering)
-    echo((">not using ls").split(" "))
     fancyRender("general", "lightgray")
     var printStr = ""
     for (var i=0; i<terminalFunctions.length; i++) {

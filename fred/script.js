3267c05a16a27b907628f746681602946ab01ae0
adriangarza <adrianhgarza@gmail.com>
2017-06-03 14:29:16 -0700

    finished first iteration

--- fred/script.js
+++ fred/script.js
@@ -5,6 +5,20 @@ var track = 0;
 var imageToggled;
 var timerUpdater;
 
+document.head = document.head || document.getElementsByTagName('head')[0];
+
+function changeFavicon(src) {
+ var link = document.createElement('link'),
+     oldLink = document.getElementById('dynamic-favicon');
+ link.id = 'dynamic-favicon';
+ link.rel = 'shortcut icon';
+ link.href = src;
+ if (oldLink) {
+  document.head.removeChild(oldLink);
+ }
+ document.head.appendChild(link);
+}
+
 function play(song, update) {
     currentSong.pause();
     currentSong.currentTime = 0;
@@ -44,7 +58,8 @@ function addSongInfo(song) {
 
     document.title = song.name +  " - " + song.artist
 
-        setBgArt(song.artPath);
+    setBgArt(song.artPath);
+    changeFavicon(song.artPath);
 }
 
 function setBgArt(imgPath) {
@@ -62,8 +77,8 @@ function setBgArt(imgPath) {
 }
 
 $('body').keyup(function(e){
-   if(e.keyCode == 32){
-       // user has pressed space
+   if(e.keyCode == 32 || e.keyCode == 75){
+       //space or k
        e.preventDefault();
        pause();
    } else if (e.keyCode == 39) {
@@ -73,6 +88,12 @@ $('body').keyup(function(e){
    } else if (e.keyCode == 37) {
        e.preventDefault();
        prev();
+   } else if (e.keyCode == 74) {
+        //j
+        currentSong.currentTime -= 10;
+   } else if (e.keyCode == 76) {
+        //l
+        currentSong.currentTime += 10;
    }
 });
 
@@ -193,196 +214,6 @@ function setAlbumArt(path) {
   }
 
 
-var cherryblossoms = {
-    audioPath : "mp3s/｢cherry blossoms explode across the dying horizon｣.mp3",
-    artPath : "art/sakuraburst.jpg",
-    name : "｢cherry blossoms explode across the dying horizon｣",
-    artist : "sakuraburst",
-    link : "https://sakuraburst.bandcamp.com",
-}
-
-var forestspirits = {
-    audioPath : "mp3s/「forest of the spirits」.mp3",
-    artPath : "art/sakuraburst.jpg",
-    name : "｢forest of the spirits｣",
-    artist : "sakuraburst",
-    link : "https://sakuraburst.bandcamp.com",
-}
-
-var haruko = {
-    audioPath : "mp3s/Airøspace - Haruko.mp3",
-    artPath : "art/airospace.jpg",
-    name : "Haruko",
-    artist : "Airøspace",
-    link : "https://airospace.bandcamp.com",
-}
-
-var allthewaydown = {
-    audioPath : "mp3s/All The Way Down.mp3",
-    artPath : "art/hexcougar.jpg",
-    name : "All The Way Down",
-    artist : "Hex Cougar",
-    link : "https://soundcloud.com/hex-cougar",
-}
-
-var fujitascale = {
-    audioPath : "mp3s/Fujita Scale.mp3",
-    artPath : "art/fujitascale.jpg",
-    name : "Fujita Scale",
-    artist : "NxxxxxS",
-    link : "https://nbbeats.bandcamp.com",
-}
-
-var heartonwave = {
-    audioPath : "mp3s/Heart On Wave.mp3",
-    artPath : "art/heartonwave.jpg",
-    name : "Video Girl (Petriform Remix)",
-    artist : "Slime Girls",
-    link : "https://slimegirls.bandcamp.com",
-}
-
-var kuro = {
-    audioPath : "mp3s/Ｋｕｒｏ｜黒 - 面白い.mp3",
-    artPath : "art/kuro.jpg",
-    name : "面白い",
-    artist : "Ｋｕｒｏ",
-    link : "https://soundcloud.com/yungkuro",
-}
-
-var tracytzu = {
-    audioPath : "mp3s/Looking For Tracy Tzu.mp3",
-    artPath : "art/trilogy.jpg",
-    name : "Looking For Tracy Tzu",
-    artist : "Carpenter Brut",
-    link : "https://carpenterbrut.bandcamp.com/album/trilogy",
-}
-
-var she = {
-    audioPath : "mp3s/She.mp3",
-    artPath : "art/cocainejesus.jpg",
-    name : "ｘ ＳＨＥ ｘ 私にとって重要がある ｘ",
-    artist : "COCAINEJESUSxPATROL1993",
-    link : "https://cocainejesus.bandcamp.com",
-}
-
-var warpstar = {
-    audioPath : "mp3s/Warpstar.mp3",
-    artPath : "art/nosummernocry.jpg",
-    name : "Warpstar (With You)",
-    artist : "Slime Girls",
-    link : "https://slimegirls.bandcamp.com",
-}
-
-var hungryghost = {
-    audioPath : "mp3s/Hungry Ghost.mp3",
-    artPath : "art/mega.jpg",
-    name : "Hungry Ghost",
-    artist : "Blank Banshee",
-    link : "https://blankbanshee.bandcamp.com",
-}
-
-var meteorblade = {
-    audioPath : "mp3s/Meteor Blade.mp3",
-    artPath : "art/mega.jpg",
-    name : "Meteor Blade",
-    artist : "Blank Banshee",
-    link : "https://blankbanshee.bandcamp.com",
-}
-
-var bonfires = {
-    audioPath : "mp3s/Bonfires.mp3",
-    artPath : "art/nosummernocry.jpg",
-    name : "Bonfires",
-    artist : "Slime Girls",
-    link : "https://slimegirls.bandcamp.com",
-}
-
-var telepath = {
-    audioPath : "mp3s/最後にホーム.mp3",
-    artPath : "art/telepath.jpg",
-    name : "最後にホーム",
-    artist : "t e l e p a t h/HKE",
-    link : "https://telepathtelepath.bandcamp.com/album/--37",
-}
-var gurrenlagann = {
-    audioPath : "mp3s/◌⚔◌ Gurren lagann ◌⚔◌.mp3",
-    artPath : "art/mechadreams.jpg",
-    name : "◌⚔◌ Gurren lagann ◌⚔◌",
-    artist : "『Drip Drop』",
-    link : "https://soundcloud.com/driptothedrop"
-}
-
-var rubber = {
-    audioPath : "mp3s/Rubber.mp3",
-    artPath : "art/moon.jpg",
-    name : "Rubber",
-    artist : "M.O.O.N",
-    link : "http://music.musicofthemoon.com/"
-}
-
-var bipp = {
-    audioPath : "mp3s/BIPP.mp3",
-    artPath : "art/bipp.jpg",
-    name : "BIPP",
-    artist : "SOPHIE",
-    link : "https://soundcloud.com/msmsmsm"
-}
-
-var possession = {
-    audioPath : "mp3s/The Possession.mp3",
-    artPath : "art/trevor.jpg",
-    name : "The Possession",
-    artist : "Trevor Something",
-    link : "https://trevorsomething.bandcamp.com/"
-}
-
-var sniper = {
-    audioPath : "mp3s/sniper rouge.mp3",
-    artPath : "art/sniper.jpg",
-    name : "Sniper Rouge feat. Emi (Satellite Young)",
-    artist : "Mitch Murder",
-    link : "https://mitchmurder.bandcamp.com/"
-}
-
-var mirage = {
-    audioPath : "mp3s/mirage.mp3",
-    artPath : "art/mirage.jpg",
-    name : "Mirage",
-    artist : "Mitch Murder",
-    link : "https://mitchmurder.bandcamp.com/"
-}
-
-var breeze = {
-    audioPath : "mp3s/breeze.mp3",
-    artPath : "art/s2.jpg",
-    name : "Breeze",
-    artist : "Mitch Murder",
-    link : "https://mitchmurder.bandcamp.com/"
-}
-
-var songs = [
-    telepath,
-    forestspirits,
-    breeze,
-    she,
-    rubber,
-    possession,
-    heartonwave,
-    warpstar,
-    hungryghost,
-    meteorblade,
-    tracytzu,
-    haruko,
-    sniper,
-    cherryblossoms,
-    allthewaydown,
-    gurrenlagann,
-    bipp,
-    kuro,
-    mirage,
-    fujitascale,
-    bonfires,
-]
 
 function switchArt(newSong, direction) {
     var artBlob = "<img src='"+newSong.artPath+"' id='newArt'>";
